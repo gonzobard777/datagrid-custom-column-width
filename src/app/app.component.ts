a62dc8915dd01923706a9ea9e72e39b938a95890
z <gonzo.bard@gmail.com>
2018-12-03 13:24:03 +0300

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import {Component, ViewChild} from '@angular/core';
+import {ClrDatagrid} from '@clr/angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  @ViewChild(ClrDatagrid) datagrid;
+
+  adverts: {
+    num: number;
+    progress: string;
+    trading_code: string;
+    debtor: string;
+    organizer: string;
+    bidding: string;
+    advert_type: string;
+    responsible: string;
+    comment: string;
+  }[] = [{
+    num: 1,
+    progress: '00000000000000000000',
+    trading_code: '9970-GMPD',
+    debtor: 'OOO "IBM 2000"',
+    organizer: 'OOO «Heaven»',
+    bidding: 'Beacon',
+    advert_type: 'public',
+    responsible: 'Authot1',
+    comment: 'Check with fennec whether it is necessary to publish the results of the first auction in the local media'
+  },
+    {
+      num: 2,
+      progress: '00000100000100001000',
+      trading_code: '90293 HDHKW',
+      debtor: 'OOO "Hello world"',
+      organizer: 'Apple',
+      bidding: 'Flat',
+      advert_type: 'first',
+      responsible: 'Author2',
+      comment: 'just comment'
+    }];
+
+  constructor() {
+    setTimeout(() => {
+      this.datagrid.resize();
+      console.log('resizee');
+    }, 5000);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ClarityModule} from '@clr/angular';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, BrowserAnimationsModule, ClarityModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
